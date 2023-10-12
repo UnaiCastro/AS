a0b6a016f2a40d6a492975e0252a624f55d8e845
UnaiCastro <83181601+UnaiCastro@users.noreply.github.com>
2023-10-13 00:39:59 +0200

    Add files via upload

---
+++ fichN.sh
@@ -0,0 +1,15 @@
+#Crear un script que cree una carpeta llamada cosas y después cree 100 ficheros vacíos llamados fich.txt dentro de ella, donde es un número entre 0 y 99.
+
+#!/bin/bash
+
+if [ ! -d cosas ]; then
+    mkdir -p cosas
+fi
+
+numero=1
+
+while [ $numero -le 100 ]
+do
+    touch cosas/fich$numero.txt
+    numero=$((numero + 1))
+done
---
+++ lsdirs.sh
@@ -0,0 +1,8 @@
+Crear un script llamado lsdirs.sh que muestre los directorios contenidos en el directorio actual.
+
+
+for item in *; do
+    if [ -d "$item" ]; then
+        echo "$item"
+    fi
+done
