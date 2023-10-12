5213bc87d1e15c733afd959eea483500249f5c1a
UnaiCastro <83181601+UnaiCastro@users.noreply.github.com>
2023-10-13 01:12:55 +0200

    Delete lsdirs.sh

--- lsdirs.sh
+++
@@ -1,8 +0,0 @@
-#Crear un script llamado lsdirs.sh que muestre los directorios contenidos en el directorio actual.
-
-
-for item in *; do
-    if [ -d "$item" ]; then
-        echo "$item"
-    fi
-done
